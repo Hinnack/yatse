b17485de4d7d75ee87a885f5862bdef7f8f08a16
Hinnack <henrik.genssen@gmail.com>
2018-07-11 22:23:41 +0200

    + info

--- modules/yatse/__init__.py
+++ modules/yatse/__init__.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from sys import version_info
+
+version = '@version@'
+
+if 'version' in version:
+    VERSION = ('a', 'b', 'c', '', 0)
+else:
+    VERSION = version.split('.')
+    VERSION.append('')
+    VERSION.append(0)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    return version
+
+def get_python_version():
+    version = '%s.%s' % (version_info[0], version_info[1])
+    if version_info[2]:
+        version = '%s.%s' % (version, version_info[2])
+    if version_info[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if version_info[3] != 'final':
+            version = '%s %s %s' % (version, version_info[3], version_info[4])
+    return version
--- modules/yatse/views.py
+++ modules/yatse/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.conf import settings
+from django import get_version as get_django_version
 from yatse.models import tickets_reports
 from yatse.forms import SearchForm
+from yatse import get_version, get_python_version
+
+import datetime
 
 def root(request):
     return render(request, "home.html", {})
 
 def info(request):
-    return render(request, "info.html", {})
+    from socket import gethostname
+
+    return render(request, "info.html", {'hostname': gethostname(), 'version': get_version(), 'date': datetime.datetime.now(), 'django': get_django_version(), 'python': get_python_version()})
 
 def table(request, **kwargs):
     search_params = {}
