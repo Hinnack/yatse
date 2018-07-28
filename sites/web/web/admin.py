180f8866e95a74d6a9d35a7d2274e23c4be5b542
Hinnack <henrik.genssen@gmail.com>
2018-07-28 16:37:10 +0200

    - admin stuff

--- sites/web/web/admin.py
+++ sites/web/web/admin.py
@@ -1,6 +1,1 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin
-from models import ticket_component
-from yatse.admin import yatseAdmin
-
-admin.site.register(ticket_component, yatseAdmin)
