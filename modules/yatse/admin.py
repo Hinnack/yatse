ead4ca92fd24d96e0f31a25a627b9827ecdae32d
Hinnack <henrik.genssen@gmail.com>
2018-07-28 16:35:36 +0200

    + admin fixes

--- modules/yatse/admin.py
+++ modules/yatse/admin.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from yatse.models import Server
 
-admin.site.register(Server)
+@admin.register(Server)
+class yatsAdmin(admin.ModelAdmin):
+    exclude = ('c_date', 'c_user', 'u_user', 'u_date', 'd_user', 'd_date', 'active_record')
+    list_filter = ('active_record',)
+
+    def save_model(self, request, obj, form, change):
+        obj.save(user=request.user)
+
+    def delete_model(self, request, obj):
+        obj.delete(user=request.user)
+
+    """
+    def changelist_view(self, request, extra_context=None):
+
+        if not request.GET.has_key('active_record__exact'):
+            q = request.GET.copy()
+            q['active_record__exact'] = 'N'
+            request.GET = q
+            request.META['QUERY_STRING'] = request.GET.urlencode()
+        return super(yatsAdmin, self).changelist_view(request, extra_context=extra_context)
+    """
